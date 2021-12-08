3f8ec444c8298b95ea6811d6f5bdb103798aa30b
CarterGunale <qq1599326217@outlook.com>
2021-12-08 19:38:57 +0800

    增加了没有角色时候的提醒

--- src/commands/r6/r6.applyrole.app.ts
+++ src/commands/r6/r6.applyrole.app.ts
@@ -6,7 +6,6 @@ class R6Applyrole extends AppCommand {
     help = '.r6 applyrole+角色'; // 帮助文字
     intro = '申请角色';
     func: AppFunc<BaseSession> = async (session) => {
-        var rid: any;
         if (session.args.length == 0) {
             session.sendCard(new Card().addTitle(this.code).addText(this.intro).addText(this.help));
             return;
@@ -15,45 +14,44 @@ class R6Applyrole extends AppCommand {
             session.sendCard(new Card().addTitle("只能在Rainbow Six小队频道使用"));
             return;
         }
-        give()
-            .then(function (num) {
-                if (num < 2) {
-                    session.user.grantRole(rid, session.guildId)
-                    session.user.grantRole(35688, session.guildId)
-                    session.send("申请成功");
-                    return;
-                } else {
-                    session.send('最多只能申请两个！')
-                    return;
-                }
-            })
-        async function give() {
-            var response = await bot.API.guildRole.index(session.guildId)
-            var num = 0;
-            return new Promise<number>((resolve) => {
-                for (var i = 0; i < response.length; i++) {
-                    if (response[i].name == session.args[0] && response[i].position >= pars.head && response[i].position <= pars.tail) {
-                        rid = response[i].roleId
-                        bot.API.guild.userList(session.guildId)
-                            .then(function (usres) {
-                                for (var j = 0; j < usres.items.length; j++) {
-                                    if (usres.items[j].id == session.userId) {
-                                        for (var k = 0; k < usres.items[j].roles.length; k++) {
-                                            for (var z = 0; z < response.length; z++) {
-                                                if (usres.items[j].roles[k] == response[z].roleId && response[z].position >= pars.head && response[z].position <= pars.head) {
-                                                    num++;
-                                                }
-
-                                            }
+        var response = await bot.API.guildRole.index(session.guildId)
+        var num = 0;
+        var flag = false;
+        for (var i = 0; i < response.length; i++) {
+            if (response[i].name == session.args[0] && response[i].position >= pars.head && response[i].position <= pars.tail) {
+                flag = true;
+                var rid = response[i].roleId
+                bot.API.guild.userList(session.guildId)
+                    .then(function (usres) {
+                        for (var j = 0; j < usres.items.length; j++) {
+                            if (usres.items[j].id == session.userId) {
+                                for (var k = 0; k < usres.items[j].roles.length; k++) {
+                                    for (var z = 0; z < response.length; z++) {
+                                        if (usres.items[j].roles[k] == response[z].roleId && response[z].position >= pars.head && response[z].position <= pars.head) {
+                                            num++;
                                         }
+
                                     }
                                 }
-                                resolve(num)
-                            })
-                    }
-                }
-            })
+                            }
+                        }
+                    })
+                    .finally(function () {
+                        if (num < 2) {
+                            session.user.grantRole(rid, session.guildId)
+                            session.user.grantRole(35688, session.guildId)
+                            session.send("申请成功");
+                            return;
+                        } else {
+                            session.send('最多只能申请两个！')
+                            return;
+                        }
+                    })
+            }
         }
+        if (!flag)
+            session.send("没有该角色")
     }
 }
+
 export const r6Applyrole = new R6Applyrole();
