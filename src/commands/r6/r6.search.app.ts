0a4b19d60b3510b516604c85abef2ec99fe5f980
CarterGunale <qq1599326217@outlook.com>
2021-12-14 19:11:00 +0800

    增加了查看更多按钮

--- src/commands/r6/r6.search.app.ts
+++ src/commands/r6/r6.search.app.ts
@@ -68,97 +68,108 @@ class R6Search extends AppCommand {
             });
         }
         function get(r6id: string) {
-            return new Promise<void>((resolve, reject) => {
-                var urln = url + r6id + '/';
-                https.get(urln, function (res: any) {
-                    var html: string = '';
-                    res.on('data', function (data: any) {
-                        html += data;
-                    });
-                    res.on('end', function () {
-                        if (html.match("RankedKDRatio") == null) {
-                            session.send("查无此人，请检查ID后重试");
-                            return;
+            var urln = url + r6id + '/';
+            https.get(urln, function (res: any) {
+                var html: string = '';
+                res.on('data', function (data: any) {
+                    html += data;
+                });
+                res.on('end', function () {
+                    if (html.match("RankedKDRatio") == null) {
+                        session.send("查无此人，请检查ID后重试");
+                        return;
+                    }
+                    else {
+                        html = html.replace(/\n/g, '');
+                        var mmr = html.match('<div class="trn-defstat__name">MMR</div><div class="trn-defstat__value">(.*?)</div>')[1].replace(',', '');
+                        var rank = html.match('<div class="trn-defstat__name">Rank</div><div class="trn-defstat__value">(.*?)</div>')[1];
+                        var kd = html.match('<div class="trn-defstat__value" data-stat="RankedKDRatio">(.*?)</div>')[1];
+                        if (flag) var src = html.match('<div class="trn-profile-header__avatar trn-roundavatar trn-roundavatar--white "><img src="(.*?)" /></div>')[1];
+                        else var src = session.user.avatar;
+                        var namer = html.match('R6Tracker - (.*?) -  Rainbow Six Siege Player Stats')[1];
+                        var level = html.match('<div class="trn-defstat__name">Level</div><div class="trn-defstat__value-stylized">(.*?)</div>')[1];
+                        var WLratio = html.match('<div class="trn-defstat__value" data-stat="PVPWLRatio">(.*?)</div>')[1];
+                        var time = html.match('<div class="trn-defstat__value" data-stat="PVPTimePlayed">(.*?)</div>')[1];
+                        try {
+                            var skd = html.match('<div class="trn-defstat__name">K/D</div><div class="trn-defstat__value">(.*?)<small>')[1];
+                        } catch (error) {
+                            skd = 'N/A'
                         }
-                        else {
-                            html = html.replace(/\n/g, '');
-                            var mmr = html.match('<div class="trn-defstat__name">MMR</div><div class="trn-defstat__value">(.*?)</div>')[1].replace(',', '');
-                            var rank = html.match('<div class="trn-defstat__name">Rank</div><div class="trn-defstat__value">(.*?)</div>')[1];
-                            var kd = html.match('<div class="trn-defstat__value" data-stat="RankedKDRatio">(.*?)</div>')[1];
-                            if (flag) var src = html.match('<div class="trn-profile-header__avatar trn-roundavatar trn-roundavatar--white "><img src="(.*?)" /></div>')[1];
-                            else var src = session.user.avatar;
-                            var namer = html.match('R6Tracker - (.*?) -  Rainbow Six Siege Player Stats')[1];
-                            var level = html.match('<div class="trn-defstat__name">Level</div><div class="trn-defstat__value-stylized">(.*?)</div>')[1];
-                            var WLratio = html.match('<div class="trn-defstat__value" data-stat="PVPWLRatio">(.*?)</div>')[1];
-                            var time = html.match('<div class="trn-defstat__value" data-stat="PVPTimePlayed">(.*?)</div>')[1];
-                            try {
-                                var skd = html.match('<div class="trn-defstat__name">K/D</div><div class="trn-defstat__value">(.*?)<small>')[1];
-                            } catch (error) {
-                                skd = 'N/A'
-                            }
-                            var arg3 = rank.replace(' ', '');
-                            var arg6 = '#B2B6BB';
-                            var rankcn;
-                            if (rank.search(/COPPER/) === 0) { arg6 = "#B30B0D"; rankcn = "紫铜"; arg3 = arg3.replace('COPPER', ''); }
-                            if (rank.search(/BRONZE/) === 0) { arg6 = "#C98B3B"; rankcn = "青铜"; arg3 = arg3.replace('BRONZE', ''); }
-                            if (rank.search(/SILVER/) === 0) { arg6 = "#B0B0B0"; rankcn = "白银"; arg3 = arg3.replace('SILVER', ''); }
-                            if (rank.search(/GOLD/) === 0) { arg6 = "#EED01E"; rankcn = "黄金"; arg3 = arg3.replace('GOLD', ''); }
-                            if (rank.search(/PLATINUM/) === 0) { arg6 = "#5BB9B3"; rankcn = "白金"; arg3 = arg3.replace('PLATINUM', ''); }
-                            if (rank.search(/DIAMOND/) === 0) { arg6 = "#BD9FF6"; rankcn = "钻石"; arg3 = arg3.replace('DIAMOND', ''); }
-                            if (rank.search(/CHAMPION/) === 0) { arg6 = "#9D385C"; rankcn = "冠军"; arg3 = arg3.replace('CHAMPION', ''); }
-                            if (rank.search(/-/) === 0) { arg6 = "#B2B6BB"; arg3 = "未定级"; }
-                            if (arg3 === 'I') arg3 = rankcn += '1';
-                            if (arg3 === 'II') arg3 = rankcn + '2';
-                            if (arg3 === 'III') arg3 = rankcn + '3';
-                            if (arg3 === 'IV') arg3 = rankcn + '4';
-                            if (arg3 === 'V') arg3 = rankcn + '5';
-                            if (flag)
-                                var card = [{
-                                    "type": "card", "theme": "secondary", "color": arg6, "size": "lg",
-                                    "modules": [{
-                                        "type": "section", "text": { "type": "kmarkdown", "content": "**:crown:" + namer + "**" },
-                                        "mode": "left", "accessory": { "type": "image", "src": src, "size": "lg" }
-                                    }, {
-                                        "type": "section", "text": {
-                                            "type": "paragraph", "cols": 3, "fields": [
-                                                { "type": "kmarkdown", "content": "**等级**\n" + level },
-                                                { "type": "kmarkdown", "content": "**段位**\n" + arg3 },
-                                                { "type": "kmarkdown", "content": "**总KD**\n" + kd },
-                                                { "type": "kmarkdown", "content": "**多人游戏时长**\n" + time },
-                                                { "type": "kmarkdown", "content": "**MMR**\n" + mmr },
-                                                { "type": "kmarkdown", "content": "**赛季KD**\n" + skd },
-                                            ]
-                                        }
-                                    }]
+                        var arg3 = rank.replace(' ', '');
+                        var arg6 = '#B2B6BB';
+                        var rankcn;
+                        if (rank.search(/COPPER/) === 0) { arg6 = "#B30B0D"; rankcn = "紫铜"; arg3 = arg3.replace('COPPER', ''); }
+                        if (rank.search(/BRONZE/) === 0) { arg6 = "#C98B3B"; rankcn = "青铜"; arg3 = arg3.replace('BRONZE', ''); }
+                        if (rank.search(/SILVER/) === 0) { arg6 = "#B0B0B0"; rankcn = "白银"; arg3 = arg3.replace('SILVER', ''); }
+                        if (rank.search(/GOLD/) === 0) { arg6 = "#EED01E"; rankcn = "黄金"; arg3 = arg3.replace('GOLD', ''); }
+                        if (rank.search(/PLATINUM/) === 0) { arg6 = "#5BB9B3"; rankcn = "白金"; arg3 = arg3.replace('PLATINUM', ''); }
+                        if (rank.search(/DIAMOND/) === 0) { arg6 = "#BD9FF6"; rankcn = "钻石"; arg3 = arg3.replace('DIAMOND', ''); }
+                        if (rank.search(/CHAMPION/) === 0) { arg6 = "#9D385C"; rankcn = "冠军"; arg3 = arg3.replace('CHAMPION', ''); }
+                        if (rank.search(/-/) === 0) { arg6 = "#B2B6BB"; arg3 = "未定级"; }
+                        if (arg3 === 'I') arg3 = rankcn += '1';
+                        if (arg3 === 'II') arg3 = rankcn + '2';
+                        if (arg3 === 'III') arg3 = rankcn + '3';
+                        if (arg3 === 'IV') arg3 = rankcn + '4';
+                        if (arg3 === 'V') arg3 = rankcn + '5';
+                        if (flag)
+                            var card = [{
+                                "type": "card", "theme": "secondary", "color": arg6, "size": "lg",
+                                "modules": [{
+                                    "type": "section", "text": { "type": "kmarkdown", "content": "**:crown:" + namer + "**" },
+                                    "mode": "left", "accessory": { "type": "image", "src": src, "size": "lg" }
+                                }, {
+                                    "type": "section", "text": {
+                                        "type": "paragraph", "cols": 3, "fields": [
+                                            { "type": "kmarkdown", "content": "**等级**\n" + level },
+                                            { "type": "kmarkdown", "content": "**段位**\n" + arg3 },
+                                            { "type": "kmarkdown", "content": "**总KD**\n" + kd },
+                                            { "type": "kmarkdown", "content": "**多人游戏时长**\n" + time },
+                                            { "type": "kmarkdown", "content": "**MMR**\n" + mmr },
+                                            { "type": "kmarkdown", "content": "**赛季KD**\n" + skd },
+                                        ]
+                                    }
+                                },
+                                {
+                                    "type": "section",
+                                    "text": {
+                                        "type": "kmarkdown",
+                                        "content": "(ins)[查看更多](" + urln + ")(ins)"
+                                    }
                                 }]
-                            else
-                                var card = [{
-                                    "type": "card", "theme": "secondary", "color": arg6, "size": "lg",
-                                    "modules": [{
-                                        "type": "section", "text": { "type": "kmarkdown", "content": "**" + namer + "**" },
-                                        "mode": "left", "accessory": { "type": "image", "src": src, "size": "lg" }
-                                    }, {
-                                        "type": "section", "text": {
-                                            "type": "paragraph", "cols": 3, "fields": [
-                                                { "type": "kmarkdown", "content": "**等级**\n" + level },
-                                                { "type": "kmarkdown", "content": "**段位**\n" + arg3 },
-                                                { "type": "kmarkdown", "content": "**总KD**\n" + kd },
-                                                { "type": "kmarkdown", "content": "**多人游戏时长**\n(spl)[解锁(CNY10)](https://afdian.net/item?plan_id=399e6166059011ec865552540025c377)(spl)" },
-                                                { "type": "kmarkdown", "content": "**MMR**\n(spl)[解锁(CNY10)](https://afdian.net/item?plan_id=399e6166059011ec865552540025c377)(spl)" },
-                                                { "type": "kmarkdown", "content": "**赛季KD**\n(spl)[解锁(CNY10)](https://afdian.net/item?plan_id=399e6166059011ec865552540025c377)(spl)" },
-                                            ]
-                                        }
-                                    }]
+                            }]
+                        else
+                            var card = [{
+                                "type": "card", "theme": "secondary", "color": arg6, "size": "lg",
+                                "modules": [{
+                                    "type": "section", "text": { "type": "kmarkdown", "content": "**" + namer + "**" },
+                                    "mode": "left", "accessory": { "type": "image", "src": src, "size": "lg" }
+                                }, {
+                                    "type": "section", "text": {
+                                        "type": "paragraph", "cols": 3, "fields": [
+                                            { "type": "kmarkdown", "content": "**等级**\n" + level },
+                                            { "type": "kmarkdown", "content": "**段位**\n" + arg3 },
+                                            { "type": "kmarkdown", "content": "**总KD**\n" + kd },
+                                            { "type": "kmarkdown", "content": "**多人游戏时长**\n(spl)[¥10解锁](https://afdian.net/item?plan_id=399e6166059011ec865552540025c377)(spl)" },
+                                            { "type": "kmarkdown", "content": "**MMR**\n(spl)[¥10解锁](https://afdian.net/item?plan_id=399e6166059011ec865552540025c377)(spl)" },
+                                            { "type": "kmarkdown", "content": "**赛季KD**\n(spl)[¥10解锁](https://afdian.net/item?plan_id=399e6166059011ec865552540025c377)(spl)" },
+                                        ]
+                                    }
+                                },
+                                {
+                                    "type": "section",
+                                    "text": {
+                                        "type": "kmarkdown",
+                                        "content": "(ins)[查看更多](" + urln + ")(ins)"
+                                    }
                                 }]
-                            session.sendCard(JSON.stringify(card));
-                        }
-                    });
-                }).on('error', function () {
-                    session.send("ERROR");
-                })
+                            }]
+                        session.sendCard(JSON.stringify(card));
+                    }
+                });
+            }).on('error', function () {
+                session.send("ERROR");
             })
         }
     }
-
 }
 export const r6Search = new R6Search();
