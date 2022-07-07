3187116248c79460f0d74f68e3824f6bfd378255
CarterGunale <qq1599326217@outlook.com>
2022-07-08 07:57:59 +0800

    Remove autorecord function

--- src/tests/init.ts
+++ src/tests/init.ts
@@ -42,59 +42,10 @@ bot.addAlias(r6Active, "激活");
 bot.addAlias(r6Auth, '认证')
 bot.connect();
 bot.logger.debug('Init Success');
-var https = require('https');
 var mysql = require('mysql');
-var url = "https://r6.tracker.network/profile/pc/";
 export var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '20060926Abc',
     database: 'bot_db'
 });
-async function getif(r6id: string) {
-    return new Promise<boolean>(async (resolve) => {
-        var urln = url + r6id;
-        https.get(urln, function (res: any) {
-            var html: string = '';
-            res.on('data', function (data: any) {
-                html += data;
-            });
-            res.on('end', function () {
-                if (html.indexOf("RankedKDRatio") !== -1)
-                    resolve(true)
-                else
-                    resolve(false)
-            })
-        })
-    })
-}
-bot.message.on('text', async (message) => {
-    if (message.type == 1 && message.channelId == '1459838591677870' && !new RegExp("[\\u4E00-\\u9FFF]+", "g").test(message.content)) {
-        if (await getif(message.content))
-            recordid(message.authorId, message.content);
-        function recordid(id: string, r6id: string) {
-            var exp = 'INSERT INTO usrlib(id,r6id) VALUES("' + id + '","' + r6id + '")'
-            connection.query(exp, function (err: any) {
-                if (err) {
-                    exp = 'UPDATE usrlib SET r6id=\'' + r6id + '\'WHERE id=' + id;
-                    connection.query(exp, function (err: any) {
-                        if (err) {
-                            var tmstp = new Date().getTime()
-                            console.log('[INSERT ERROR] - ', err.message, ' [ID] - ', tmstp);
-                            bot.API.message.create(1, message.channelId, '[INSERT ERROR] - [ID] - ' + tmstp);
-                        }
-                        else {
-                            bot.API.message.create(1, message.channelId, '查询到此ID并换绑：' + r6id, '', message.authorId);
-                            //updateList(id)
-
-                        }
-                    })
-                }
-                else {
-                    bot.API.message.create(1, message.channelId, '查询到此ID并绑定：' + r6id, '', message.authorId);
-                    //updateList(id)
-                }
-            })
-        }
-    }
-})
