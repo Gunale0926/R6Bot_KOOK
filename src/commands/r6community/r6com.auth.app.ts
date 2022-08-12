945108a4a74ea14dfb51b799c72b286c758f5c39
CarterGunale <qq1599326217@outlook.com>
2022-08-12 19:51:49 +0800

    Update authorize condition
    	modified:   src/commands/r6community/r6com.auth.app.ts

--- src/commands/r6community/r6com.auth.app.ts
+++ src/commands/r6community/r6com.auth.app.ts
@@ -1,4 +1,4 @@
-import { AppCommand, AppFunc, BaseSession, Card} from '../..';
+import { AppCommand, AppFunc, BaseSession, Card } from '../..';
 import { connection } from '../../tests/init';
 import axios from 'axios';
 class R6Auth extends AppCommand {
@@ -11,7 +11,7 @@ class R6Auth extends AppCommand {
         if (session.guildId != '3128617072930683') {
             session.sendCard(
                 new Card().addTitle('只能在Rainbow Six小队频道使用')
-                );
+            );
             return;
         }
         var r6id = await searchid(session.userId);
@@ -36,46 +36,46 @@ class R6Auth extends AppCommand {
         function get(r6id: string) {
             var stats: any, highestMMR;
             axios
-            .get(
-                'http://127.0.0.1:9099/getUser.php?appcode=thisisthecode&name=' +
-                r6id
+                .get(
+                    'http://127.0.0.1:9099/getUser.php?appcode=thisisthecode&name=' +
+                    r6id
                 )
-            .then(function (res: any) {
-                stats = res.data.players[Object.keys(res.data.players)[0]];
-            })
-            .then(function () {
-                var nodata: any;
-                try {
-                    nodata = stats.error.message;
-                } catch {}
-                if (nodata == 'User not found!') {
-                    session.send('查无此人！请检查ID后重试！');
-                    return;
-                }
+                .then(function (res: any) {
+                    stats = res.data.players[Object.keys(res.data.players)[0]];
+                })
+                .then(function () {
+                    var nodata: any;
+                    try {
+                        nodata = stats.error.message;
+                    } catch { }
+                    if (nodata == 'User not found!') {
+                        session.send('查无此人！请检查ID后重试！');
+                        return;
+                    }
 
-                try {
-                    highestMMR = stats.max_mmr;
-                } catch (error) {
-                    session.send('未进行过排位！');
-                    return;
-                }
-                var highestRank;
-                if (highestMMR < 1600) highestRank = '紫铜';
-                else if (highestMMR < 2100) highestRank = '黄铜';
-                else if (highestMMR < 2600) highestRank = '白银';
-                else if (highestMMR < 3200) highestRank = '黄金';
-                else if (highestMMR < 4100) highestRank = '白金';
-                else if (highestMMR < 5000) highestRank = '钻石';
-                else highestRank = '冠军';
-                session.send(
-                    'ID：' +
-                    r6id +
-                    '\n本赛季最高段位：' +
-                    highestRank +
-                    '\n请等待审核！'
+                    try {
+                        highestMMR = stats.max_mmr;
+                    } catch (error) {
+                        session.send('未进行过排位！');
+                        return;
+                    }
+                    var highestRank;
+                    if (highestMMR < 1600) highestRank = '紫铜';
+                    else if (highestMMR < 2100) highestRank = '黄铜';
+                    else if (highestMMR < 2600) highestRank = '白银';
+                    else if (highestMMR < 3200) highestRank = '黄金';
+                    else if (highestMMR < 4100) highestRank = '白金';
+                    else if (highestMMR < 5000) highestRank = '钻石';
+                    else highestRank = '冠军';
+                    session.send(
+                        'ID：' +
+                        r6id +
+                        '\n本赛季最高段位：' +
+                        highestRank +
+                        '\n请发送段位截图并等待审核！'
                     );
-                return;
-            });
+                    return;
+                });
         }
     };
 }
