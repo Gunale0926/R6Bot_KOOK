717773f119a5260014f7679d4c595e1bbb6806a0
root <qq1599326217@outlook.com>
2022-11-16 21:46:53 +0800

    a

--- src/tests/init.ts
+++ src/tests/init.ts
@@ -52,7 +52,3 @@ export var connection = mysql.createConnection({
 	password: '20060926Abc',
 	database: 'bot_db'
 });
-axios.get("http://bot.gekj.net/api/v1/online.bot", { headers: { uuid: '1ccd9294-24bf-4a82-ac99-6dc6ce02838b' } })
-let job = schedule.scheduleJob('*/5 * * * *', () => {
-	axios.get("http://bot.gekj.net/api/v1/online.bot", { headers: { uuid: '1ccd9294-24bf-4a82-ac99-6dc6ce02838b' } })
-});
